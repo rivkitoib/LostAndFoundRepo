971259322cbb36d488a587140b6fe8a93fa1e356
rivto <41346932+rivkitoib@users.noreply.github.com>
2019-12-19 00:24:57 +0200

    Rivky's initial commit

---
+++ LostAndFound/Models/DbHandle.cs
@@ -0,0 +1,30 @@
+﻿namespace LostAndFound.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+
+    public partial class DbHandle : DbContext
+    {
+   
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+
+
+
+        public virtual DbSet<HeadCategory> headCategories { get; set; }
+        public virtual DbSet<Find> finds { get; set; }
+        public virtual DbSet<Location> locations { get; set; }
+        public virtual DbSet<SubCategory> subCategories { get; set; }
+        public virtual DbSet<Archive> archive { get; set; }
+    }
+
+}
---
+++ LostAndFound/Models/SubCategory.cs
@@ -0,0 +1,24 @@
+
+
+namespace LostAndFound.Models
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class SubCategory
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SubCategory()
+        {
+            this.finds = new HashSet<Find>();
+        }
+    
+        public int id { get; set; }
+        public int categoryId { get; set; }
+        public string name { get; set; }
+    
+        public virtual HeadCategory Categories { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Find> finds { get; set; }
+    }
+}
