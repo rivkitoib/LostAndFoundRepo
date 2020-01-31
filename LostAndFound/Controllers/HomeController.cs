a8d94bdb17988357d420427cfeffe037fd626290
Tzippy Glantz <tzippyglantz@gmail.com>
2020-01-31 03:29:47 +0200

    index view & first steps to filter

--- LostAndFound/Controllers/HomeController.cs
+++ LostAndFound/Controllers/HomeController.cs
@@ -11,9 +11,55 @@ namespace LostAndFound.Controllers
     {
         // GET: Home
         DbHandle DB = new DbHandle();
-        public ActionResult Index()
+        public ActionResult Index(string category)
         {
-            return View();
+            ViewBag.category = DB.headCategories.ToList();
+            ViewBag.place = DB.locations.ToList();
+
+            //ViewBag.category2.First(x => x.Id == item.idCategory).Name;
+            if (category == null)
+            {
+                var t = (from f in DB.finds.ToList()
+                         join sc in DB.subCategories.ToList()
+                         on f.subCategory.id equals sc.id
+                         join p in DB.locations.ToList()
+                         on f.location.Id equals p.Id
+                         join c in DB.headCategories.ToList()
+                         on sc.headCategory.Id equals c.Id
+
+                         select new View()
+                         {
+                             id = f.id,
+                             idSubCategory = f.subCategory.id,
+                             hebrewDate = f.hebrewDate,
+                             name = f.finderName,
+                             email = f.email,
+                             notes = f.notes,
+                             date = f.dateFound.ToString(),
+                             description = f.description,
+                             cellphone = f.cellphone,
+                             subcategoryname = sc.name,
+                             categoryId = sc.headCategory.Id,
+                             categoryName = c.Name,
+                             PlaceOrEvent = p.PlaceOrEvent
+                         });
+                ViewBag.tbl = t.ToList();
+
+                return View();
+            }
+
+            List<string> lsName = new List<string>();
+            if (category != "הכל")
+            {
+                int id = DB.headCategories.First(x => x.Name == category).Id;
+                var ls = DB.subCategories.Where(x => x.headCategory.Id == id).ToList();
+
+                foreach (var item in ls)
+                {
+                    lsName.Add(item.name);
+                }
+            }
+            return Json(lsName, JsonRequestBehavior.AllowGet);
         }
         [HttpGet]
         public ActionResult CreateFind(string headCategory)
@@ -27,7 +73,7 @@ namespace LostAndFound.Controllers
             else
                 id = 1;//default
             DB.subCategories.Where(sub => sub.id == id).ToList();
-            //var ls = db.SubCategory.Where(x => x.categoryId == id).Where(x => x.name != "אחר").ToList();
+            //var ls = DB.SubCategory.Where(x => x.categoryId == id).Where(x => x.name != "אחר").ToList();
 
             //foreach (var item in ls)
             //{
@@ -43,9 +89,89 @@ namespace LostAndFound.Controllers
         [HttpPost]
         public ActionResult CreateFind()
         {
-            
+
             return View();
         }
+
+        public ActionResult Search(string subCategory, string place, DateTime fromDate, DateTime toDate, string text, string hiddenCategory)
+        {
+
+            var findsList = DB.finds.Where(x => x != null);
+            int helpId;
+            //sorting by place
+            if (place != "הכל")
+            {
+                helpId = DB.locations.First(x => x.PlaceOrEvent == place).Id;
+                findsList = findsList.Where(x => x.location.Id == helpId);
+            }
+            //sorting by category or subCategory
+            if (hiddenCategory != null && hiddenCategory != "הכל")
+            {
+                if (subCategory != "הכל")
+                {
+                    helpId = DB.subCategories.First(x => x.name == subCategory).id;
+                    findsList = findsList.Where(x => x.subCategory.id == helpId);
+                }
+                else
+                {
+                    helpId = DB.headCategories.First(x => x.Name == hiddenCategory).Id;
+                    var ITSsubCategories = DB.subCategories.Where(x => x.headCategory.Id == helpId);
+
+                    findsList = from a in ITSsubCategories
+                                join b in findsList
+                                on a.id equals b.subCategory.id
+                                select b;
+                }
+            }
+            //sorting by date
+            findsList = findsList.Where(x => DateTime.Compare(x.dateFound, fromDate) >= 0);
+            findsList = findsList.Where(x => DateTime.Compare(toDate, x.dateFound) >= 0);
+            //findsList = findsList.Where(x => x.dateFound.Year >= fromDate.Year && x.dateFound.Month >= fromDate.Month && x.dateFound.Day >= fromDate.Day);
+            //findsList = findsList.Where(x => x.dateFound.Year <= toDate.Year && x.dateFound.Month <= toDate.Month && x.dateFound.Day <= toDate.Day);
+            // findsList = findsList.Where(x => ==true);
+            // findsList = findsList.Where(x => IsGreaterOrEqual(toDate,x.dateFound)==true);
+            //findsList = from a in findsList
+            //            where IsGreaterOrEqual(a.dateFound, fromDate)
+            //            select a;
+            //findsList = from a in findsList
+            //            where IsGreaterOrEqual(toDate, a.dateFound)
+            //            select a;
+            //sorting by text
+            if (text != "")
+            {
+                findsList = findsList.Where(x => x.description.Contains(text) || x.notes.Contains(text));
+            }
+            var t = (from f in DB.finds.ToList()
+                     join sc in DB.subCategories.ToList()
+                     on f.subCategory.id equals sc.id
+                     join p in DB.locations.ToList()
+                     on f.location.Id equals p.Id
+                     join c in DB.headCategories.ToList()
+                     on sc.headCategory.Id equals c.Id
+                     select new View()
+                     {
+                         id = f.id,
+                         idSubCategory = f.subCategory.id,
+                         hebrewDate = f.hebrewDate,
+                         name = f.finderName,
+                         email = f.email,
+                         notes = f.notes,
+                         date = f.dateFound.ToString(),
+                         description = f.description,
+                         cellphone = f.cellphone,
+                         subcategoryname = sc.name,
+                         categoryId = sc.headCategory.Id,
+                         categoryName = c.Name,
+                         PlaceOrEvent = p.PlaceOrEvent
+                     });
+            ViewBag.tbl = t.ToList();
+
+
+            return PartialView();
+        }
+
+
+
 
     }
 }
---
+++ LostAndFound/Models/View.cs
@@ -0,0 +1,26 @@
+﻿//בס"ד
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LostAndFound.Models
+{
+    public class View
+    {
+        public int id { get; set; }
+        public int idSubCategory { get; set; }
+        public string hebrewDate { get; set; }
+        public string date { get; set; }
+        public string PlaceOrEvent { get; set; }
+        public string name { get; set; }
+        public string myclass { get; set; }
+        public string description { get; set; }
+        public string notes { get; set; }
+        public string cellphone { get; set; }
+        public string email { get; set; }
+        public int categoryId { get; set; }
+        public string subcategoryname { get; set; }
+        public string categoryName { get; set; }
+    }
+}
