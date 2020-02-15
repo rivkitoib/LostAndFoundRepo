bfdc72c7b14545eb4574019a2063f2d2acd72030
Tzippy Glantz <tzippyglantz@gmail.com>
2020-02-15 19:47:30 +0200

    search to cards

---
+++ DAL/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LostAndFound.Models
+{
+    public class Settings
+    {
+       public string password;
+      public  int freqInDays;
+    }
+}
